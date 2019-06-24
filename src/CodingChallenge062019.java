01dcaedf0cf81df50f5e0fafc213aafa07e4bf6d
Synesaelv <vanessa.synesael@autodata.net>
2019-06-24 09:22:22 -0400

    coding challenge june 20

--- src/CodingChallenge062019.java
+++ src/CodingChallenge062019.java
@@ -1,12 +1,37 @@
 import java.util.*;
 
+/**
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ *
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ */
 public class CodingChallenge062019 {
 
+    /**
+     * My approach :: divide and conquer.
+     * Sort the list and add the first to the last item in the list.
+     * If the sum is lower than the target, increase the index of the lowest to the next number in the array.
+     * If the sum is higher than the target, decrease the index of the highest number in the array to the next one.
+     * If the sum equals the target return true.
+     * Continue to loop through the array as long as we aren't hitting the outer bounds of the array.
+     *
+     * @param arr given array
+     * @param target target sum
+     * @return is there a combination that is the target
+     */
     public static boolean findTarget(int[] arr, int target) {
         int lowest = 0;
         int highest = arr.length-1;
-        Arrays.sort(arr);
 
+        // if the array is bigger than zero, sort it, else return false
+        if (arr.length > 0) {
+            Arrays.sort(arr);
+        }
+        else {
+            return false;
+        }
+
+        // continue until we hit the outer bounds of the array
         while (lowest < arr.length && highest >= 0) {
             int sum = arr[lowest] + arr[highest];
             if (sum == target) {
